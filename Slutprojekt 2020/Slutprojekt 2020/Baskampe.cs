58f1169ac8d641d342127d260e18d158ea336b00
MartinNyberg <55439200+MartinNyberg@users.noreply.github.com>
2020-03-10 12:49:23 +0100

    Create Baskampe.cs

---
+++ Slutprojekt 2020/Slutprojekt 2020/Baskampe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slutprojekt_2020
+{
+    class Baskampe
+    {
+        int champhp = 500;
+        int champattack = 50;
+        float champcrittc = 0.02f;
+    }
+}
